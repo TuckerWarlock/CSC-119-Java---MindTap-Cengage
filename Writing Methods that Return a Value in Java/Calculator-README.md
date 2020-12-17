69ee8361f85957c85b9605c1c5d5364ca11c33c4
TuckerWarlock <tuckerpw01123@gmail.com>
2020-12-16 18:24:48 -0700

    Added Writing Methods that return value .java and README

---
+++ Writing Methods that Return a Value in Java/Calculator-README.md
@@ -0,0 +1,12 @@
+# Writing Methods that Return a Value in Java
+
+### Summary
+In this lab, you complete a partially written Java program that includes a method that returns a value. 
+The program is a simple calculator that prompts the user for two numbers and an operator ( +, −, *, /,or% ). 
+The two numbers and the operator are passed to the method where the appropriate arithmetic operation is performed. 
+The result is then returned to the main() method where the arithmetic operation and result are displayed. 
+For example, if the user enters 3, 4, and *, the following is displayed:
+
+```
+3.00 * 4.00 = 12.00
+```
---
+++ Writing Methods that Return a Value in Java/Calculator.java
@@ -0,0 +1,62 @@
+// Calculator.java - This program performs arithmetic, ( +. -, *. /, % ) on two numbers
+// Input:  Interactive.
+// Output:  Result of arithmetic operation
+
+import java.util.Scanner;
+
+public class Calculator
+{
+  public static void main(String[] args) 
+  {
+    double numberOne, numberTwo;                
+    String numberOneString, numberTwoString;
+    String operation;
+    double myResult = 0;
+    double result = 0.00;
+    Scanner input = new Scanner(System.in);
+            
+    System.out.println("Enter the first number: ");
+    numberOneString = input.nextLine();
+    numberOne = Double.parseDouble(numberOneString);
+    System.out.println("Enter the second number: ");
+    numberTwoString = input.nextLine();
+    numberTwo = Double.parseDouble(numberTwoString); 
+    System.out.println("Enter an operator (+.-.*,/,%): ");
+    operation = input.nextLine();
+    
+    // Call performOperation method here    
+    result = performOperation(numberOne, numberTwo, operation);
+
+    System.out.format("%.2f",numberOne);
+    System.out.print(" " + operation + " ");
+    System.out.format("%.2f", numberTwo);
+    System.out.print(" = ");
+    System.out.format("%.2f", result);
+  
+    System.exit(0);
+
+  } // End of main() method.
+  
+  
+  // Write performOperation method here.
+  public static double performOperation(double numberOne, double numberTwo, String operation){
+      double result = 0;
+        if(operation.equals("+")){
+            result = numberOne + numberTwo;
+        }
+        if (operation.equals("-")){
+            result = numberOne - numberTwo;
+        }
+        if(operation.equals("*")){
+            result = numberOne * numberTwo;
+        }
+        if(operation.equals("/")){
+            result = numberOne / numberTwo;
+        }
+        if(operation.equals("%")){
+            result = numberOne % numberTwo;
+        }
+        return result;
+    }
+
+} // End of Calculator class.
